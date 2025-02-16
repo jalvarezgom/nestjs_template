8539401095f919f86bbc2db961a763614757b17e
Jancel <ja.alvarezgom@gmail.com>
2025-02-16 17:38:49 +0100

    feat: add initial project structure with authentication and user management part2

---
+++ src/core/enums/environment.enum.ts
@@ -0,0 +1,11 @@
+export enum EnvironmentTypes {
+  DEV = 'DEV',
+  TEST = 'TEST',
+  PROD = 'PROD',
+}
+
+export enum DatabaseProviderType {
+  DEV = 'databaseDevProvider',
+  TEST = 'databaseTestProvider',
+  PROD = 'databaseProdProvider',
+}
